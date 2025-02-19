86cd0a54841bb8b49869c6ff19b4c5339378b626
unknown <guillermoperronelopez@gmail.com>
2025-02-19 19:22:40 -0300

    Agregando espacios en el json

--- core/main.py
+++ core/main.py
@@ -5,10 +5,12 @@ def landing_to_raw():
     df = pd.read_csv('../db/landing/ecommerce/orders/839012383812.csv') #TODO CAMBIAR ESTO
     df_metadata_columns = df[['user_id', '_job_batch_runtime', '_job_batch_id']]
 
-    df_json = df.to_json()
-
+    #df_json = df.to_json()
+    df_json = "\n".join(df.apply(lambda row: row.to_json(), axis=1))
+    df_json_2 = "\n".join(df_metadata_columns.apply(lambda row: row.to_json(), axis=1))
+    print(df_json_2)
 
     with open("output.txt", "w") as file:
-        file.write(df_json)
+        file.write(df_json_2)
 
 landing_to_raw()
