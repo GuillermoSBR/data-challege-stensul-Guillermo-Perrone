4119cf76ea343cd0c31f61d0d9c6303232c329dc
unknown <guillermoperronelopez@gmail.com>
2025-02-19 19:26:02 -0300

    primera acercamiento a la solucion usando el to_json para crear una nueva columna

--- core/main.py
+++ core/main.py
@@ -3,14 +3,11 @@ import pandas as pd
 
 def landing_to_raw():
     df = pd.read_csv('../db/landing/ecommerce/orders/839012383812.csv') #TODO CAMBIAR ESTO
-    df_metadata_columns = df[['user_id', '_job_batch_runtime', '_job_batch_id']]
-
-    #df_json = df.to_json()
-    df_json = "\n".join(df.apply(lambda row: row.to_json(), axis=1))
-    df_json_2 = "\n".join(df_metadata_columns.apply(lambda row: row.to_json(), axis=1))
-    print(df_json_2)
+    df['raw'] = df.apply(lambda row: row.to_json(), axis=1)
+    df_metadata_columns = df[['user_id', '_job_batch_runtime', '_job_batch_id','raw']]
+    df_json = "\n".join(df_metadata_columns.apply(lambda row: row.to_json(), axis=1))
 
     with open("output.txt", "w") as file:
-        file.write(df_json_2)
+        file.write(df_json)
 
 landing_to_raw()
