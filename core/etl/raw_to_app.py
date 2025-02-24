811af315089ec02b09126166ec6601533a7f1755
unknown <guillermoperronelopez@gmail.com>
2025-02-23 21:00:58 -0300

    Se movio todo a transformations y la tabla de inputs está terminada

--- core/etl/raw_to_app.py
+++ core/etl/raw_to_app.py
@@ -1,7 +1,7 @@
 from transformations import *
 import confuse
 from pathlib import Path
-import json
+
 
 project_path = str(Path(__file__).resolve().parents[2])
 config = confuse.Configuration('Data_challenge_stensul', __name__)
@@ -9,13 +9,11 @@ config.set_file(project_path + '/core/config/config.yml')
 
 def main_execution(raw):
     raw_path = project_path + config['directories']['raw'].get()
+    app_path = project_path + config['directories']['app'].get()
     df = pd.read_json(raw_path + raw, lines=True)
-    df["raw"] = df["raw"].apply(json.loads)
-    df_expanded = pd.json_normalize(df["raw"])
-    print(df_expanded.columns)
-    #print(df_expanded)
-    #df_final = df.drop(columns=["raw"]).join(df_expanded)
-    #print(df_final)
+
+    inputs_df = inputs_table_transform(df)
+    inputs_df.to_parquet(app_path+'inputs_table.parquet', engine='pyarrow')
 
     return "Hello World!"
 
--- core/etl/transformations.py
+++ core/etl/transformations.py
@@ -1,8 +1,19 @@
 import pandas as pd
+import json
+from datetime import datetime
 
 def landing_to_raw_transform(df):
     df['raw'] = df.apply(lambda row: row.to_json(), axis=1)
     df_metadata_columns = df[['user_id', '_job_batch_runtime', '_job_batch_id', 'raw']]
     df_json = "\n".join(df_metadata_columns.apply(lambda row: row.to_json(), axis=1))
     return df_json
+
+def inputs_table_transform(df):
+    df["raw"] = df["raw"].apply(json.loads)
+    df_expanded = pd.json_normalize(df["raw"])
+    df_filtered = df_expanded.filter(items=['user_id', 'order_number', 'total_price_usd', 'total_price'])
+    df_filtered["id"] = range(1, len(df) + 1)
+    df_filtered["timestamp"] = datetime.now().timestamp()
+    df_filtered = df_filtered.reindex(columns= ['id', 'user_id', 'order_number', 'total_price_usd', 'total_price', 'timestamp'])
+    return df_filtered
 
