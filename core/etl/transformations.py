ba7213ca40d2655a6e6fca920cc048fc19e443e9
unknown <guillermoperronelopez@gmail.com>
2025-02-24 14:34:18 -0300

    agregados tipos de datos al transform

--- core/etl/transformations.py
+++ core/etl/transformations.py
@@ -13,7 +13,9 @@ def inputs_table_transform(df):
     df_expanded = pd.json_normalize(df["raw"])
     df_filtered = df_expanded.filter(items=['user_id', 'order_number', 'total_price_usd', 'total_price'])
     df_filtered["id"] = range(1, len(df) + 1)
-    df_filtered["timestamp"] = datetime.now().timestamp()
+    df_filtered["timestamp"] = int(datetime.now().timestamp())
     df_filtered = df_filtered.reindex(columns= ['id', 'user_id', 'order_number', 'total_price_usd', 'total_price', 'timestamp'])
+    df_filtered = df_filtered.astype({'id': 'int', 'user_id':'int', 'order_number':'int', 'total_price_usd':'float',
+                                      'total_price':'float', 'timestamp':'string'})
     return df_filtered
 
